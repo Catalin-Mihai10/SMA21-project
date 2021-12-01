3c18bc479b50c7a2f0ae448af65d3c66e6c626db
Ancuta Catalin-Mihai <ancuta.catalin.mihai@gmail.com>
2021-12-01 19:01:25 +0200

    created start_up,login/register page

--- app/src/main/java/com/upt/cti/aplicatiecomandat/MainActivity.java
+++ app/src/main/java/com/upt/cti/aplicatiecomandat/MainActivity.java
@@ -3,12 +3,14 @@ package com.upt.cti.aplicatiecomandat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.startup_background);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
